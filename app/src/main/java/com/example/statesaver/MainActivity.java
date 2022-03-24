2948b8d4f5795cd45344de416d9b1bf5fe52e139
Shashank Kumar <shashankrust@gmail.com>
2022-03-24 16:22:41 +0530

    save instance state

--- app/src/main/java/com/example/statesaver/MainActivity.java
+++ app/src/main/java/com/example/statesaver/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.statesaver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int mCounter  = 0;
+    private TextView textView;
+    private final String KEY = "key";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+        findViewById(R.id.button).setOnClickListener(view->{
+            mCounter++;
+            reflectClick();
+        });
+    }
+
+    private void reflectClick(){
+        textView.setText(String.valueOf(mCounter));
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d("MAIn", "onSaveInstanceState: ");
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY, mCounter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mCounter = savedInstanceState.getInt(KEY);
+        reflectClick();
     }
 }
